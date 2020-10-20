af6ba3e79f47b68ef2db98cafa5bcfac4815bfff
DF-Damm <69846839+DF-Damm@users.noreply.github.com>
2020-10-20 20:09:40 +0200

    for synthesizing new views (needs trained model)

--- models/ViewSynthesis.py
+++ models/ViewSynthesis.py
@@ -15,10 +15,13 @@ class ViewSynthesis(object):
     def combine(self,weight=0.5):
         if weight<0 or weight >0:
             raise ValueError('Weight should be float between 0 and 1.')
-        mu1, log_var1 = model.encode(self.input[0])
-        mu2, log_var2 = model.encode(self.input[1])
+        mu1, log_var1 = self.model.encode(self.input[0])
+        mu2, log_var2 = self.model.encode(self.input[1])
         z1 = self.model.reparameterize(mu1,log_var1)
         z2 = self.model.reparameterize(mu2,log_var2)
         return self.model.decode(weight*z1+(1-weight)*z2).detach()
     def compare(self, weight = 0.5):
-        return: F.mse_loss(self.combine(weight), self.target)
+        res = self.combine(weight)
+        err = F.mse_loss(res, self.target)
+        return err
+        
