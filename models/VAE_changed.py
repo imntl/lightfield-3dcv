8f07e2362ddeac51f7056965099c970523841656
SabineSchmidt <sabineschmidt4@gmx.net>
2020-10-23 22:43:02 +0200

    inlcusion added

--- models/VAE_changed.py
+++ models/VAE_changed.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from typing import List, Callable, Union, Any, TypeVar, Tuple
 from .ResidualBlocks import ResidualBlockEncoder, ResidualBlockDecoder
 # from torch import tensor as Tensor
 Tensor = TypeVar('torch.tensor')
